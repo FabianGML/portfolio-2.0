ef8225f58a7e59c73fed83765258984fd9bb559b
Fabian Gutierrez <fabiangtml@gmail.com>
2025-03-05 11:12:27 -0800

    Cambiando a tailwind 4 y astro 5.4.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.fabiangutierrez.dev/',
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false
     }
-  },
-
-  integrations: [tailwind()]
+  }
 })
